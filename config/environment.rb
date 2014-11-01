99e6e5fab8db0dbca6aca35ca1f1657688d95f66
Matthias Loker <mmloker@gmail.com>
2014-11-01 16:00:27 +0100

    restructure app

--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,5 @@
 require "bundler/setup"
+require 'csv'
 
 Bundler.require
 
---
+++ lib/day.rb
@@ -0,0 +1,12 @@
+class Day
+
+  attr_accessor :start_time, :end_time, :pause_time, :home_study_time
+
+	def initialize(day_string)
+    @start_time = day_string[1]
+    @end_time = day_string[2]
+    @pause_time = day_string[3]
+    @home_study_time = day_string[4]
+  end
+
+end

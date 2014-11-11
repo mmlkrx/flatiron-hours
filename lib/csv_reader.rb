810ee233c2ea9765f32929b14a08db9074104bb6
Matthias Loker <mmloker@gmail.com>
2014-11-11 13:28:19 +0100

    start convert day method

--- lib/csv_reader.rb
+++ lib/csv_reader.rb
@@ -1,9 +1,10 @@
 class CsvReader
 
-  attr_accessor :days
+  attr_accessor :days, :converted_days
 
   def initialize(file_path)
     self.days = []
+    self.converted_days = []
 
     CSV.foreach(file_path, "r:ISO-8859-1") do |row|    
       self.days << row
@@ -11,9 +12,34 @@ class CsvReader
 
   end
 
-  def convert_datetime
+  def export_days
+    e_days = {}
+    converted_days.each do |day|
+      e_days["#{day[0]}"] = [day[1], day[2], day[3], day[4]]
+    end
+    e_days
+  end
+
+  def convert_all_days
      # ["6/30/14", "8:15", "20:00", "210", "0"],
      # ["7/1/14", "8:15", "8:20", "120", "60"],
+     days.each do |day|
+       converted_days << convert(day)
+     end
+  end
+
+  def convert(day)
+    day.collect do |e|
+      if date?(e)
+        # convert to date
+      elsif time?(e)
+        # convert to datetime
+      elsif minutes?(e)
+        e.to_i
+      else
+        raise TypeError("Can't convert: #{e}")  
+      end
+    end
   end
 
   def date?(str)
